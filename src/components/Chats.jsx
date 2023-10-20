73c79946a62a7008dd733c3b54a2fa6b88bd1bd0
ChronaNavarrete <bastian.navarrete@usm.cl>
2023-10-19 23:36:19 -0300

    Chats (all)

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,14 @@
-function Chats () {
-  return (
-    <div>
-      <h1>CHATS</h1>
-    </div>
-  )
+import React from "react";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import "../chats.css"
+
+
+function Chats() {
+  return <div className="chats">
+    <Sidebar/>
+    <Chat/>
+  </div>
 }
 
 export default Chats;
